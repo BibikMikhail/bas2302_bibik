2e43769cdabd0ee047cf3677e81cdafe9477701d
Mikhail Bibik <i@m-bibik.ru>
2025-10-04 18:37:44 +0300

    RBPO Bibik lab v1.0

---
+++ src/main/java/ru/mfa/bas2302_bibik/Bas2302BibikApplication.java
@@ -0,0 +1,13 @@
+package ru.mfa.bas2302_bibik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bas2302BibikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bas2302BibikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mfa/bas2302_bibik/Bas2302BibikApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mfa.bas2302_bibik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bas2302BibikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
